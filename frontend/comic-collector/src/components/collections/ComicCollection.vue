9752b1731aba88efd4a3fc2267245b0580f7d68a
forrest rojas <forrest.rojas@gmail.com>
2019-04-09 12:30:49 -0400

    modified collections vue files

--- frontend/comic-collector/src/components/collections/ComicCollection.vue
+++ frontend/comic-collector/src/components/collections/ComicCollection.vue
@@ -1,22 +1,21 @@
 <template>
-    <article class="collection">
-        <picture class="collection-img">
-            <source v-bind:srcset="collection.imageName" />
-            <img v-bind:src="collection.imageName" alt="Collection image." />
-        </picture>
-        <h4 class="collection-name">{{collection.title}}</h4>
-        <p class="username">{{userName}}</p>
-        <p class="collection-description">{{collection.description}}</p>
-    </article>
+	<article class="collection">
+		<picture class="collection-img">
+			<source v-bind:srcset="collection.imageName" />
+			<img v-bind:src="collection.imageName" alt="Collection image." />
+		</picture>
+		<h4 class="collection-name">{{collection.title}}</h4>
+		<p class="username">{{collection.userName}}</p>
+		<p class="collection-description">{{collection.description}}</p>
+	</article>
 </template>
 
 <script>
 export default {
-    name: 'comic-collection',
-    props: {
-        collection: Object,
-        userName: String
-    }
+	name: 'comic-collection',
+	props: {
+			collection: Object,
+	}
 }
 </script>
 
