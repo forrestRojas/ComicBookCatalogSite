14a56eb7ededdcfdcef7504766fe63872dfa7121
forrest rojas <forrest.rojas@gmail.com>
2019-04-09 11:43:59 -0400

    created comic collection interface

---
+++ backend/SampleApi/DAL/IComicCollectionDAO.cs
@@ -0,0 +1,24 @@
+﻿using SampleApi.Models;
+using System.Collections.Generic;
+
+namespace SampleApi.DAL
+{
+    /// <summary>
+    /// An interface for the ComicCollectionDAO.
+    /// </summary>
+    public interface IComicCollectionDAO
+    {
+        /// <summary>
+        /// graps all the collections from the DB.
+        /// </summary>
+        /// <returns>A List of Collections</returns>
+        IList<ComicCollection> GetCollections();
+
+        /// <summary>
+        /// Saves a list of collections to the DB.
+        /// </summary>
+        /// <param name="collections">The List to be saved.</param>
+        /// <returns>A Bool indicating if the transaction was succesfull.</returns>
+        bool SaveCollections(IList<ComicCollection> collections);
+    }
+}
