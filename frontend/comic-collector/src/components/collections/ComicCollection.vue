3d06bd298222100e0be4d80e7105770e25ffd3f6
forrest rojas <forrest.rojas@gmail.com>
2019-04-10 10:30:53 -0400

    Added UserName Component

--- frontend/comic-collector/src/components/collections/ComicCollection.vue
+++ frontend/comic-collector/src/components/collections/ComicCollection.vue
@@ -5,14 +5,19 @@
 			<img v-bind:src="collection.imageName" alt="Collection image." />
 		</picture>
 		<h4 class="collection-name">{{collection.title}}</h4>
-		<p class="username">{{collection.userName}}</p>
+		<user-name v-bind:userId="collection.userId"></user-name>
 		<p class="collection-description">{{collection.description}}</p>
 	</article>
 </template>
 
 <script>
+import UserName from '@/components/user/UserName.vue';
+
 export default {
 	name: 'comic-collection',
+	components: {
+		UserName
+	},
 	props: {
 			collection: Object,
 	},
