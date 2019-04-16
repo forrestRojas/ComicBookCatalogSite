65c8e9dbdafe62f1a94b4a75349d7e70f30b9827
Ryan Pearce <pearcer15@gmail.com>
2019-04-16 11:14:33 -0400

    Created a stats dao

---
+++ backend/SampleApi/DAL/IStatsDAO.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SampleApi.Models;
+
+namespace SampleApi.DAL
+{
+    /// <summary>
+    /// An interface for statistics
+    /// </summary>
+    public interface IStatsDAO
+    {
+        /// <summary>
+        /// A list of recently updated collections
+        /// </summary>
+        /// <returns>A list of collections</returns>
+        IList<ComicCollection> RecentlyUpdated();
+
+        /// <summary>
+        /// A list of the largest collcetions
+        /// </summary>
+        /// <returns>A list of collcetions</returns>
+        IList<ComicCollection> LargestCollection();
+
+        /// <summary>
+        /// A list of the most collceted comics
+        /// </summary>
+        /// <returns>A likst of comic books</returns>
+        IList<ComicBook> MostPopularComic();
+
+        /// <summary>
+        /// The total number of comics in the database
+        /// </summary>
+        /// <returns>The number of comics</returns>
+        int TotalComics();
+
+        /// <summary>
+        /// A list of comics from a specified date range
+        /// </summary>
+        /// <returns>A list of comics</returns>
+        IList<ComicBook> ComicsFromDateRange();
+
+        /// <summary>
+        /// A list of the publishers that shows up the most in the collection
+        /// </summary>
+        /// <returns>Name of publisher and count of appearances</returns>
+        IList<(string, int)> MostPopularPublisher();
+
+        /// <summary>
+        /// A list of user's with the biggest total number of comics
+        /// </summary>
+        /// <returns>A list of users</returns>
+        IList<UserDisplay> UserWithMostComics();
+    }
+}
---
+++ backend/SampleApi/DAL/StatsSQLDAO.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SampleApi.Models;
+
+namespace SampleApi.DAL
+{
+    public class StatsSQLDAO : IStatsDAO
+    {
+        private readonly string connectionString;
+
+        public StatsSQLDAO(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+
+        public IList<ComicBook> ComicsFromDateRange()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<ComicCollection> LargestCollection()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<ComicBook> MostPopularComic()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<(string, int)> MostPopularPublisher()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<ComicCollection> RecentlyUpdated()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int TotalComics()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<UserDisplay> UserWithMostComics()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
