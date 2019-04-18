7781cf673c9caaef7fe2d91f0d6ff3bd8239ea4c
Peter Scanniello <pscanniello216@gmail.com>
2019-04-18 14:43:14 -0400

    made my user branch home.vue exactly like dev home.vue

--- frontend/comic-collector/src/views/Home.vue
+++ frontend/comic-collector/src/views/Home.vue
@@ -1,11 +1,12 @@
 <template>
-    <main id="home">
-        <article>
-            <top-user></top-user>
-            <largest-collections></largest-collections>
-            <most-popular></most-popular>
-        </article>
-    </main>
+<main id="home">
+<article>
+<top-user></top-user>
+<largest-collections></largest-collections>
+<most-popular></most-popular>
+
+</article>
+</main>
 </template>
 
 <script>
@@ -14,11 +15,12 @@ import TopUser from '@/components/Stats/TopUser.vue';
 import LargestCollections from '@/components/Stats/LargestCollections.vue';
 import MostPopular from '@/components/Stats/MostPopular.vue';
 
+
 export default {
 components:{
-    TopUser,
-    LargestCollections,
-    MostPopular,
+TopUser,
+LargestCollections,
+MostPopular
 }
 
 }
