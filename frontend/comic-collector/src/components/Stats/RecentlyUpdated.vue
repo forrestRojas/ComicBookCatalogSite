f681878f29c6cef2b0dfc5bb8a23d734c9fad86d
Ryan Pearce <pearcer15@gmail.com>
2019-04-18 12:12:39 -0400

    Fixed h2 on recently updated component

---
+++ frontend/comic-collector/src/components/Stats/RecentlyUpdated.vue
@@ -0,0 +1,36 @@
+<template>
+    <article>
+    <h2>Recently Updated Collections</h2>
+    <collections-grid v-bind:comicCollections="largestcollections"></collections-grid>
+
+    </article>
+</template>
+
+<script>
+import CollectionsGrid from '@/components/collections/CollectionsGrid.vue';
+
+export default {
+name: "largest-collections",
+components:{
+    CollectionsGrid
+},
+data(){
+    return {
+        recentlyedited: []
+        
+    }
+},
+created(){
+
+    fetch(`${process.env.VUE_APP_REMOTE_API}/statistics/recentactivity`, {
+        method: "GET",
+    })
+    .then(response => response.json())
+    .then(json => {this.recentlyedited = json});
+}
+}
+</script>
+
+<style>
+
+</style>
