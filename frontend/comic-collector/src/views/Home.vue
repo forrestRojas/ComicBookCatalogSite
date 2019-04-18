f8729cde0601e17e923fe406c790553af01e23e5
Peter Scanniello <pscanniello216@gmail.com>
2019-04-18 14:40:40 -0400

    removed publisher, collection, total colection from home.vue

--- frontend/comic-collector/src/views/Home.vue
+++ frontend/comic-collector/src/views/Home.vue
@@ -4,9 +4,6 @@
             <top-user></top-user>
             <largest-collections></largest-collections>
             <most-popular></most-popular>
-            <publisher></publisher>
-            <total-collections></total-collections>
-            <totalComics></totalComics>
         </article>
     </main>
 </template>
@@ -16,18 +13,12 @@
 import TopUser from '@/components/Stats/TopUser.vue';
 import LargestCollections from '@/components/Stats/LargestCollections.vue';
 import MostPopular from '@/components/Stats/MostPopular.vue';
-import Publisher from '@/components/Stats/Publisher.vue';
-import TotalCollections from '@/components/Stats/TotalCollections.vue';
-import TotalComics from '@/components/Stats/TotalComics.vue';
 
 export default {
 components:{
     TopUser,
     LargestCollections,
     MostPopular,
-    Publisher,
-    TotalCollections,
-    TotalComics
 }
 
 }
